f90497b9f3da67abfc37a4778fff82919b447b36
Paul Watts <paulcwatts@gmail.com>
2012-01-23 13:21:10 -0800

    Add the ability to disallow certain usernames
    Set from a setting.
    Also, change _get_username => get_username. It was private, but now
    it's public.

--- auth_utils/utils.py
+++ auth_utils/utils.py
@@ -1,26 +1,38 @@
 from django.contrib.auth.models import User
 
-def _get_username(basename):
+from django.conf import settings
+
+
+def is_allowed_username(username):
+    disallowed = getattr(settings, 'AUTH_DISALLOWED_USERNAMES', [])
+    return username.lower() not in disallowed
+
+
+def get_username(basename):
+    disallowed = getattr(settings, 'AUTH_DISALLOWED_USERNAMES', [])
+
     # Truncate the basename to 27 characters
     # (The username is only 30 characters)
-    basename=basename[:27]
-    try:
-        # First just try their username
-        User.objects.get(username=basename)
-    except User.DoesNotExist:
-        return basename
+    basename = basename[:27]
+    if basename.lower() not in disallowed:
+        try:
+            # First just try their username
+            User.objects.get(username__iexact=basename)
+        except User.DoesNotExist:
+            return basename
 
-    i=0
+    i = 0
     while True:
         try:
-            username=basename+str(i)
-            User.objects.get(username=username)
+            username = basename + str(i)
+            if username.lower() not in disallowed:
+                User.objects.get(username__iexact=username)
             i = i + 1
         except User.DoesNotExist:
             return username
 
+
 def email_to_username(email):
     # Generate a unique username from the email address
     basename = email.split('@')[0].lower()
-    return _get_username(basename)
-
+    return get_username(basename)
